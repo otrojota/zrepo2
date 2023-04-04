080303de74c135b123cabc849716013aa6d8f158
Jorge Jimenez <jjimenez@zonar.cl>
2023-04-04 15:06:17 -0400

    DimTable multicolumnas y totales

--- www/zdashboards/DimTable.js
+++ www/zdashboards/DimTable.js
@@ -38,6 +38,10 @@ class DimTable extends ZDashboardElement {
                     let r = map[row.dim.code];
                     if (r) {
                         r.resultado = row.resultado;
+                        r.min = row.min;
+                        r.max = row.max;
+                        r.n = row.n;
+                        r.value = row.value;
                     }
                 }
                 data = rows;
@@ -50,16 +54,23 @@ class DimTable extends ZDashboardElement {
             if (this.q.accum == "n") unit = "N°";
             else unit = this.q.variable.options?this.q.variable.options.unit:"S/U";
             let titulo = this.options.tituloColumnaDimension || "";
+            let extraHeaders = "";
+            if (this.options.accum2 && this.options.accum2 != "no") extraHeaders += `<td class="text-end">${this.getTituloAccum(this.options.accum2)}</td>`;
+            if (this.options.accum3 && this.options.accum3 != "no") extraHeaders += `<td class="text-end">${this.getTituloAccum(this.options.accum3)}</td>`;
+            if (this.options.accum4 && this.options.accum4 != "no") extraHeaders += `<td class="text-end">${this.getTituloAccum(this.options.accum4)}</td>`;
+            if (this.options.accum5 && this.options.accum5 != "no") extraHeaders += `<td class="text-end">${this.getTituloAccum(this.options.accum5)}</td>`;            
             let html = `
                 <table class="table table-dark table-striped">
                     <thead>
                         <tr>
                             <td>${titulo}</td>
                             <td class="text-end">${unit}</td>
+                            ${extraHeaders}
                         </tr>
                     </thead>
                     <tbody>
             `;
+            let totales = {sum:0, n:0};
             for (let row of data) {
                 html += `<tr>`;
                 html += `   <td>${row.dim.name}</td>`;
@@ -68,10 +79,33 @@ class DimTable extends ZDashboardElement {
                     stVal = (row.resultado || 0).toLocaleString();
                 }
                 html += `   <td class="text-end">${stVal}</td>`;
+
+                if (this.options.accum2 && this.options.accum2 != "no") html += `<td class="text-end">${this.extraeAccum(this.options.accum2, row)}</td>`;
+                if (this.options.accum3 && this.options.accum3 != "no") html += `<td class="text-end">${this.extraeAccum(this.options.accum3, row)}</td>`;
+                if (this.options.accum4 && this.options.accum4 != "no") html += `<td class="text-end">${this.extraeAccum(this.options.accum4, row)}</td>`;
+                if (this.options.accum5 && this.options.accum5 != "no") html += `<td class="text-end">${this.extraeAccum(this.options.accum5, row)}</td>`;
+
                 html += `</tr>`;
+                if (row.n !== undefined) {
+                    totales.n += row.n;
+                    totales.sum += row.value;
+                    if (totales.min === undefined || row.min < totales.min) totales.min = row.min;
+                    if (totales.max === undefined || row.max > totales.max) totales.max = row.max;
+                }
+            }
+            let tfoot = "";
+            if (this.options.totalsRow) {
+                tfoot += "<tfoot><tr><td>Totales</td>";
+                tfoot += `<td class="text-end">${this.getTotal(this.q.accum, totales)}</td>`;
+                if (this.options.accum2 && this.options.accum2 != "no") tfoot += `<td class="text-end">${this.getTotal(this.options.accum2, totales)}</td>`;
+                if (this.options.accum3 && this.options.accum3 != "no") tfoot += `<td class="text-end">${this.getTotal(this.options.accum3, totales)}</td>`;
+                if (this.options.accum4 && this.options.accum4 != "no") tfoot += `<td class="text-end">${this.getTotal(this.options.accum4, totales)}</td>`;
+                if (this.options.accum5 && this.options.accum5 != "no") tfoot += `<td class="text-end">${this.getTotal(this.options.accum5, totales)}</td>`;
+                tfoot += "</tr></tfoot>";
             }
             html += `
                     </tbody>
+                    ${tfoot}
                 </table>
             `;
             this.tableContainer.innerHTML = html;
@@ -80,6 +114,38 @@ class DimTable extends ZDashboardElement {
         } catch(error) {
             console.error(error);
             this.showError(error.toString());
+        }
+    }
+
+    getTituloAccum(accum) {
+        switch(accum) {
+            case "n":   return "Nª";
+            case "sum": return this.q.variable.options?this.q.variable.options.unit:"S/U";
+            case "min": return "Min";
+            case "max": return "Max";
+            case "avg": return "Prom.";
+            default: return "??: " + accum;
+        }
+    }
+    extraeAccum(accum, row) {
+        switch(accum) {
+            case "n":   return (row.n || 0);
+            case "sum": return (row.value || 0).toLocaleString();
+            case "min": return (row.min !== undefined?row.min.toLocaleString():"");
+            case "max": return (row.max !== undefined?row.max.toLocaleString():"");
+            case "avg": return (row.n !== undefined && row.n > 0?(row.value / row.n).toLocaleString():"");
+            default: return "??: " + accum;
+        }
+    }
+
+    getTotal(accum, totales) {
+        switch(accum) {
+            case "n":   return totales.n;
+            case "sum": return totales.sum.toLocaleString();
+            case "min": return (totales.min !== undefined?totales.min.toLocaleString():"");
+            case "max": return (totales.max !== undefined?totales.max.toLocaleString():"")
+            case "avg": return (totales.n !== undefined && totales.n > 0?(totales.sum / totales.n).toLocaleString():"");
+            default: return "??: " + accum;
         }
     }
 
